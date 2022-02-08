cbcb676c32f2b2fa04700576ea6e5bc70ebc91da
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2022-02-09 00:48:16 +0300

    Add BoardManager

---
+++ Assets/Scripts/Managers/BoardManager.cs
@@ -0,0 +1,19 @@
+namespace Managers
+{
+    public class BoardManager : BaseManager<BoardManager>
+    {
+        public override void Initialize()
+        {
+        }
+        public override void UnInitialize()
+        {
+        }
+
+        public override void Subscribe()
+        {
+        }
+        public override void UnSubscribe()
+        {
+        }
+    }
+}
